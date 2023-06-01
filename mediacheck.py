dc7a8a6e2082fd1b010e8e0ac519cf9279556b20
Finn H <f.hoffhenke@fs-itsysteme.de>
2023-06-01 13:45:14 +0200

    Added DB recheck

--- mediacheck.py
+++ mediacheck.py
@@ -4,6 +4,7 @@ import hashlib
 import mysql.connector
 from loguru import logger
 import yaml
+import time
 
 
 def calculate_file_hash(file_path):
@@ -19,33 +20,49 @@ def check_video_integrity(path, db_connection):
     for root, dirs, files in os.walk(path):
         for file in files:
             if file.lower().endswith(('.mp4', '.avi', '.mkv', '.mov')):  # Add more video formats as needed
+                start_time = time.time()
                 file_path = os.path.join(root, file)
                 file_hash = calculate_file_hash(file_path)
 
-                cursor.execute("SELECT COUNT(*) FROM checked_files WHERE file_hash = %s", (file_hash,))
+                cursor.execute("SELECT COUNT(*) FROM checked_files WHERE file_hash = %s AND result = 'OK'", (file_hash,))
                 result = cursor.fetchone()
                 if result[0] > 0:
                     logger.info(f'Skipping {file_path} (already checked)')
                     continue
-
+                
+                ffmpeg_result = ""
                 try:
                     subprocess.run(['ffmpeg', '-v', 'error', '-i', file_path, '-f', 'null', '-'],
                                    stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, check=True)
+                    ffmpeg_result = "OK"
                     logger.info(f'{file_path}: OK')
-
-                    # Store the file in the database
-                    cursor.execute("INSERT INTO checked_files (file_hash, file_name) VALUES (%s, %s)",
-                                   (file_hash, file))
-                    db_connection.commit()
                 except subprocess.CalledProcessError as e:
+                    ffmpeg_result = "ERROR"
                     logger.error(f'{file_path}: Error - {e.stderr.decode().strip()}')
+
+                end_time = time.time()
+
+                logger.debug("Took %d" % ((end_time - start_time)))
+
+                cursor.execute("SELECT COUNT(*) FROM checked_files WHERE file_hash = %s AND result = 'ERROR'", (file_hash,))
+                result = cursor.fetchone()
+                if result[0] > 0:
+                    logger.info(f'{file_path}: Rechecked')
+                    cursor.execute("UPDATE checked_files SET last_check = CURRENT_TIMESTAMP(), result = %s, duration = %s WHERE file_hash = %s",
+                                    (ffmpeg_result, str(end_time - start_time), file_hash,))
+                    db_connection.commit()
+                else:
+                    # Store the file in the database
+                    cursor.execute("INSERT INTO checked_files (file_hash, file_name, result, duration) VALUES (%s, %s, %s, %s)",
+                                    (file_hash, file, ffmpeg_result, str(end_time - start_time),))
+                    db_connection.commit()
 
     cursor.close()
     db_connection.close()
 
 if __name__ == "__main__":
     config = yaml.safe_load(open("config.yml"))
-    path = input("Please enter the path to the folder to check: ")
+    path = config['check']['path']
 
     db_host = config['db']['host']
     db_user = config['db']['user']
